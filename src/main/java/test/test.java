9acc0fe2bdb63356a82306fbd4a5955881b17494
jonghunbaek <qor6028@naver.com>
2023-01-16 14:31:10 +0900

    test

---
+++ src/main/java/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
+public class test {
+
+}
